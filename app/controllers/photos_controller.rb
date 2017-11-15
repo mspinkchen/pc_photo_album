ed6ad95dd8f1522da30b79869177bfc6f75f7a70
mspink <arpinkchen@gmail.com>
2017-11-15 21:53:16 +0800

    update feature

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,10 +1,8 @@
 class PhotosController < ApplicationController
+  before_action :set_photo, :only => [:edit, :show, :update, :destroy]
+
   def index
     @photos = Photo.all
-  end
-
-  def show
-    @photo = Photo.find(params[:id])
   end
 
   def new
@@ -21,13 +19,9 @@ def create
 end
 end
 
-def edit
-@photo = Photo.find(params[:id])
-end
 
 
 def update
-  @photo = Photo.find(params[:id])
   if @photo.update_attributes(photo_params)
     redirect_to photo_path(@photo)
   else
@@ -37,12 +31,17 @@ end
 
 
 def destroy
-  @photo = Photo.destroy(params[:id])
+  @photo.destroy
   redirect_to photos_path
-  
 end
 
 private
+
+def set_photo
+  @photo = Photo.find(params[:id])
+end
+
+
 
 def photo_params
   params.require(:photo).permit(:title, :date, :description)
